609f4988cb4126b2552f8ab84faa25f005667dfa
Rosa Palaci <61998089+Rosa-Palaci@users.noreply.github.com>
2022-03-23 11:54:02 -0600

    Se agrego el contador de las veces que volteas una carta

--- memory.py
+++ memory.py
@@ -10,14 +10,20 @@ Exercises:
 """
 
 from random import *
+from tkinter import CENTER
 from turtle import *
-
+"""se agrego counter"""
+from typing import Counter 
 from freegames import path
 
 car = path('car.gif')
+rubb = ("python", "c++", "html","css","java","flutter","c","cpp","py","django","db","cs","code","pro","stress","macos","windows","linux","data","science","msft","google","fb","intern","yahoo","linkedin","binary","sort","merge","bigO","$","bug","python","c++","html","css","java","flutter","c","cpp","py","django","db","cs","code","pro","stress","macos","windows","linux","data","science","msft","google","fb","intern","yahoo","linkedin","binary","sort","merge","bigO","$","bug")
+tiles = list(rubb)
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
+nClicks = 0
+pares = 0
 
 
 def square(x, y):
@@ -45,6 +51,9 @@ def xy(count):
 
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
+    global nClicks
+    global pares
+    nClicks += 1
     spot = index(x, y)
     mark = state['mark']
 
@@ -54,6 +63,7 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        pares += 1
 
 
 def draw():
@@ -73,9 +83,16 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 25, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'), align = 'center')
+
+    penup()
+    goto(-200,200)
+    write(nClicks, font=20)
+    if pares == 32:
+        goto(-100,200)
+        write("Juego Completado", font=20)
 
     update()
     ontimer(draw, 100)
